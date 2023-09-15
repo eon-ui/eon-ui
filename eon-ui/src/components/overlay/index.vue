59fc5d20caa44e84943bc8fc95a29901ea8a390e
GuoJikun <qwerboy0912@gmail.com>
2023-09-15 16:06:51 +0800

    ✨ feat: 初步增加overlay组件

---
+++ eon-ui/src/components/overlay/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'EonOverlay'
+})
+
+const props = defineProps({
+  modelValue: Boolean,
+  showMask: {
+    type: Boolean,
+    default: true
+  },
+  maskClosable: {
+    type: Boolean,
+    default: true
+  },
+  appendToBody: Boolean,
+  transition: {
+    type: String,
+    default: 'eon-fade'
+  }
+})
+</script>
+
+<template>
+  <teleport to="body" :disabled="props.appendToBody">
+    <transition name="">
+      <div class="eon-overlay">
+        <div class="eon-overlay-mask" v-if="props.showMask"></div>
+
+        <slot />
+      </div>
+    </transition>
+  </teleport>
+</template>
